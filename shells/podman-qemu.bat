e26de5fdf49ff437da3730c2cb1a64b768ff6992
Arthur Sengileyev <arthur.sengileyev@gmail.com>
2022-08-24 13:45:40 +0300

    Prepare all bundle

---
+++ shells/podman-qemu.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+set QCW_DIR=%~dp0..
+
+set PATH=%QCW_DIR%\qemu;%QCW_DIR%\xz;%QCW_DIR%\podman;%PATH%
+
+set CONTAINERS_MACHINE_PROVIDER=QEMU
+
+set CONTAINERS_HELPER_BINARY_DIR=%QCW_DIR%\podman
+
+cmd /k "ECHO Welcome to Podman with Qemu shell"
