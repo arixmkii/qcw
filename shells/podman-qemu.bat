76da1ce35f1d2a76016c1ebf2db5d613959f7dd8
Arthur Sengileyev <arthur.sengileyev@gmail.com>
2022-12-14 22:54:19 +0200

    Updated podman build

--- shells/podman-qemu.bat
+++ shells/podman-qemu.bat
@@ -2,7 +2,7 @@
 
 set QCW_DIR=%~dp0..
 
-set PATH=%QCW_DIR%\qemu;%QCW_DIR%\xz;%QCW_DIR%\podman;%LOCALAPPDATA%\Programs\podman-desktop;%PROGRAMFILES%\qemu;%PATH%
+set PATH=%QCW_DIR%\qemu;%QCW_DIR%\xz;%LOCALAPPDATA%\Programs\podman-desktop;%PROGRAMFILES%\qemu;%PATH%
 
 set CONTAINERS_MACHINE_PROVIDER=QEMU
 
