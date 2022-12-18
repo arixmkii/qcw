425ad1a25567b288e933952a50c68d8f9b30831e
Arthur Sengileyev <arthur.sengileyev@gmail.com>
2022-12-18 16:07:59 +0200

    Reworked qcw utils

---
+++ qcw-utils.ps1
@@ -0,0 +1,57 @@
+$target=$args[0]
+
+if ([string]::IsNullOrEmpty($target)) { $target = "qcw-utils" }
+
+if (Test-Path -Path $target) {
+    Write-Output "Target forlder '$target' already exists"
+    exit 1
+}
+
+
+$qemuBat = @"
+@ECHO OFF
+
+set QCW_DIR=%~dp0..
+
+set PATH=%QCW_DIR%\xz;%LOCALAPPDATA%\Programs\podman-desktop;%PROGRAMFILES%\qemu;%PATH%
+
+set CONTAINERS_MACHINE_PROVIDER=QEMU
+
+set CONTAINERS_HELPER_BINARY_DIR=%QCW_DIR%\podman
+
+cmd /k "ECHO Welcome to Podman with Qemu shell"
+"@
+
+$wslBat = @"
+@ECHO OFF
+
+set QCW_DIR=%~dp0..
+
+set PATH=%QCW_DIR%\xz;%LOCALAPPDATA%\Programs\podman-desktop;%PATH%
+
+set CONTAINERS_MACHINE_PROVIDER=WSL
+
+set CONTAINERS_HELPER_BINARY_DIR=%QCW_DIR%\podman
+
+cmd /k "ECHO Welcome to Podman with WSL shell"
+"@
+
+
+$shellsTarget = Join-Path $target "shells"
+$xzTarget = Join-Path $target "xz"
+New-Item -Path $target -ItemType Directory | Out-Null
+
+New-Item -Path $shellsTarget -ItemType Directory | Out-Null
+New-Item -Path $shellsTarget -Name "podman-qemu.bat" -ItemType File -Value $qemuBat | Out-Null
+New-Item -Path $shellsTarget -Name "podman-wsl.bat" -ItemType File -Value $wslBat | Out-Null
+
+$xzUrl = "https://tukaani.org/xz/xz-5.2.9-windows.zip"
+$xzDl = New-TemporaryFile
+$xzExpand = New-TemporaryFile
+Invoke-WebRequest -Uri $xzUrl -OutFile $xzDl.FullName
+Remove-Item $xzExpand.FullName
+Expand-Archive $xzDl.FullName -DestinationPath $xzExpand.FullName
+$xzBin = Join-Path $xzExpand "bin_x86-64"
+Copy-Item $xzBin $xzTarget -Recurse
+
+Write-Output "'qcw-utils' to '$target' folder installation completed"
--- xz/download-xz.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -o xz.zip https://tukaani.org/xz/xz-5.2.9-windows.zip
-unzip -d dist xz.zip
